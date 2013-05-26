66ee4aae5bb33680e6f0c4e14f113f6ad18001e8
Martin Bilski <martinb@criticue.com>
2013-05-26 11:42:10 +0200

    ...

--- fbapp/app.rb
+++ fbapp/app.rb
@@ -81,7 +81,7 @@ get "/" do
     # for other data you can always run fql
     @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
   end
-  erb :index
+  erb :ask
 end
 
 # used by Canvas apps - redirect the POST to be a regular GET
